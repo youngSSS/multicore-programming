eeec974cc63366b4eff685a1ca52f77658ca6dab
YoungSSS <young4127s@naver.com>
2021-10-15 17:07:24 +0900

    Wait-free Snapshot

--- parallel-join-processing/Joiner.cpp
+++ parallel-join-processing/Joiner.cpp
@@ -68,6 +68,33 @@ void Joiner::join(QueryInfo& query, int threadIdx)
 	// cerr << query.dumpText() << endl;
 	set<unsigned> usedRelations;
 
+	unordered_map<int, int> relationCounter;
+
+	for (auto pInfo : query.predicates) {
+		relationCounter[pInfo.left.relId] += 1;
+		relationCounter[pInfo.right.relId] += 1;
+	}
+
+	for (auto pInfo : query.predicates) {
+		if (relationCounter[pInfo.left.relId] == 1 && relationCounter[pInfo.right.relId] == 1) {
+			auto left = addScan(usedRelations, pInfo.left, query);
+			auto right = addScan(usedRelations, pInfo.right, query);
+			unique_ptr<Operator> root = make_unique<Join>(move(left), move(right), pInfo);
+
+			Checksum checkSum(move(root), query.selections);
+			checkSum.run();
+
+			stringstream out;
+			auto& results = checkSum.checkSums;
+			for (unsigned i = 0; i < results.size(); ++i) {
+				out << (checkSum.resultSize == 0 ? "NULL" : to_string(results[i]));
+				if (i < results.size() - 1)
+					out << " ";
+			}
+			out << "\n";
+		}
+	}
+
 	// We always start with the first join predicate and append the other joins to it (--> left-deep join trees)
 	// You might want to choose a smarter join ordering ...
 	auto& firstJoin = query.predicates[0];
@@ -75,14 +102,9 @@ void Joiner::join(QueryInfo& query, int threadIdx)
 	auto right = addScan(usedRelations, firstJoin.right, query);
 	unique_ptr<Operator> root = make_unique<Join>(move(left), move(right), firstJoin);
 
-//	Utils::printLog("JOINER-JOIN-LOOP-P_INFO", "0th pInfo");
-//	Utils::printLog("JOINER-JOIN-LOOP-LEFT", left + "." + leftInfo.colId);
-//	Utils::printLog("JOINER-JOIN-LOOP-RIGHT", rightInfo.binding + "." + rightInfo.colId);
-//	Utils::printNewLine();
-
 	// Predicate 연산만 수행한다. (1 <= i < predicate.size()에 대해서 join을 수행하는 이유)
 	// Filter는 Predicate 연산 수행시, Filter 조건에 맞는 Relation과 Column이 있는 경우에만 수행된다.
-	// Predicate 연산 수행 전 Filter를 먼저 적용하여 탐색 범위를 줄이고 Predicate 연산을 수핸한다.
+	// Predicate 연산 수행 전 Filter를 먼저 적용하여 탐색 범위를 줄이고 Predicate 연산을 수행한다.
 	for (unsigned i = 1; i < query.predicates.size(); ++i) {
 		auto& pInfo = query.predicates[i];
 		auto& leftInfo = pInfo.left;
